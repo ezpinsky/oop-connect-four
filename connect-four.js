894f5567778b036d4d48446d6b1bba72b3df13b5
ramses <ramses.romero.jr@gmail.com>
2020-09-10 15:50:31 -0400

    Finished Start a New Game section

--- connect-four.js
+++ connect-four.js
@@ -1,19 +1,16 @@
-class Game {
-	constructor(player1, player2) {
-		this.player1 = player1;
-		this.player2 = player2;
+import Game from './game.js';
+
+var game = undefined;
+
+function updateUI() {
+	const boardHolder = document.getElementById("board-holder");
+	if (game === undefined) {
+		boardHolder.classList.add("is-invisible");
+	} else {
+        boardHolder.classList.remove("is-invisible");
+        document.getElementById("game-name").innerHTML = game.getName()
 	}
 }
-game = undefined;
-
-// function updateUI() {
-// 	const boardHolder = document.getElementById("board-holder");
-// 	if (game === undefined) {
-// 		boardHolder.classList.add("is-invisible");
-// 	} else {
-// 		boardHolder.classList.remove("is-invisible");
-// 	}
-// }
 
 window.addEventListener("DOMContentLoaded", (event) => {
 	const newGame = document.getElementById("new-game");
@@ -22,8 +19,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
 	const player2 = document.getElementById("player-2-name");
 
 	players.addEventListener("keyup", (e) => {
-		console.log(`player 1 is ${player1.value}
-        player 2 is ${player2.value}`);
 		if (player1.value !== "" && player2.value !== "") {
 			newGame.disabled = false;
 		} else {
@@ -31,13 +26,13 @@ window.addEventListener("DOMContentLoaded", (event) => {
 		}
 	});
 
-	// newGame.addEventListener("click", (e) => {
-	// 	game = new Game(player1.value, player2.value);
-	// 	player1.value = "";
-	// 	player2.value = "";
-	// 	newGame.disabled = true;
-	// 	updatedUI();
-	// });
+	newGame.addEventListener("click", (e) => {
+		game = new Game(player1.value, player2.value);
+		player1.value = "";
+		player2.value = "";
+		newGame.disabled = true;
+		updateUI();
+	});
 
 	// const currentPlayer = "red";
 	// const placePiece = document.getElementById("click-target")
