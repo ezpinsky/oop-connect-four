9fe5c2a84b3184e9f2e4c32cc174e0aadfcdf1b3
Ezra <ezpinsky@gmail.com>
2020-09-10 14:06:23 -0400

    added newGame btn func and updateUI func

--- connect-four.js
+++ connect-four.js
@@ -1,34 +1,50 @@
 class Game {
-    constructor(player1, player2){
-        this.player1 = player1;
-        this.player2 = player2;
-    }
+	constructor(player1, player2) {
+		this.player1 = player1;
+		this.player2 = player2;
+	}
 }
-window.addEventListener("DOMContentLoaded", event =>{
-    game = undefined;
+game = undefined;
 
-    const player1 = document.getElementById("player-1-name");
-    const player2 = document.getElementById("player-2-name");
+// function updateUI() {
+// 	const boardHolder = document.getElementById("board-holder");
+// 	if (game === undefined) {
+// 		boardHolder.classList.add("is-invisible");
+// 	} else {
+// 		boardHolder.classList.remove("is-invisible");
+// 	}
+// }
 
-    player1.addEventListener("keyup", e =>{
+window.addEventListener("DOMContentLoaded", (event) => {
+	const newGame = document.getElementById("new-game");
+	const players = document.getElementById("form-holder");
+	const player1 = document.getElementById("player-1-name");
+	const player2 = document.getElementById("player-2-name");
 
-        if(player1.value !== undefined && player2.value !== undefined){
-        document.getElementById("new-game").disabled = false;
-        }else{
-            document.getElementById("new-game").disabled = true;
-        }
-    });
+	players.addEventListener("keyup", (e) => {
+		console.log(`player 1 is ${player1.value}
+        player 2 is ${player2.value}`);
+		if (player1.value !== "" && player2.value !== "") {
+			newGame.disabled = false;
+		} else {
+			newGame.disabled = true;
+		}
+	});
 
+	// newGame.addEventListener("click", (e) => {
+	// 	game = new Game(player1.value, player2.value);
+	// 	player1.value = "";
+	// 	player2.value = "";
+	// 	newGame.disabled = true;
+	// 	updatedUI();
+	// });
 
+	// const currentPlayer = "red";
+	// const placePiece = document.getElementById("click-target")
+	// placePiece.addEventListener("click", e =>{
+	//     console.log(e.target.id);
+	// })
+	// function isColumnFull (){
 
-
-
-    // const currentPlayer = "red";
-    // const placePiece = document.getElementById("click-target")
-    // placePiece.addEventListener("click", e =>{
-    //     console.log(e.target.id);
-    // })
-    // function isColumnFull (){
-
-    // }
-})
+	// }
+});
