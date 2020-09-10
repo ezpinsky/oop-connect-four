68b9cdba2c4de7543b39384246ddcceefcc7bcbf
ramses <ramses.romero.jr@gmail.com>
2020-09-10 16:32:46 -0400

    finished indicating currentPlayer

--- connect-four.js
+++ connect-four.js
@@ -2,21 +2,38 @@ import Game from "./game.js";
 
 var game = undefined;
 
-function updateUI() {
+function updateUI(placePieceSpot) {
 	const boardHolder = document.getElementById("board-holder");
 	if (game === undefined) {
 		boardHolder.classList.add("is-invisible");
 	} else {
 		boardHolder.classList.remove("is-invisible");
 		document.getElementById("game-name").innerHTML = game.getName();
-	}
+    }
+    if(placePieceSpot !== undefined){
+
+        if(game.currentPlayer === 2 ){
+            placePieceSpot.classList.remove('black');
+            placePieceSpot.classList.add('red');
+        }else{
+            placePieceSpot.classList.remove('red');
+            placePieceSpot.classList.add('black');
+        }
+    }
+
 }
 
 window.addEventListener("DOMContentLoaded", (event) => {
 	const newGame = document.getElementById("new-game");
 	const players = document.getElementById("form-holder");
 	const player1 = document.getElementById("player-1-name");
-	const player2 = document.getElementById("player-2-name");
+    const player2 = document.getElementById("player-2-name");
+
+    document.getElementById("click-targets")
+    .addEventListener("click", e=>{
+        game.playInColumn();
+        updateUI(e.currentTarget)
+    })
 
 	players.addEventListener("keyup", (e) => {
 		if (player1.value !== "" && player2.value !== "") {
